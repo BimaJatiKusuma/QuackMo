1e40be455fdeb8e641ea7c3d23aa1cd3ed42b333
BimaJatiKusuma <bimajatifzth03@gmail.com>
2023-04-11 21:15:19 +0700

    progress UI peternak transaksi

--- lib/peternak/peternak_transaksi.dart
+++ lib/peternak/peternak_transaksi.dart
@@ -1,4 +1,80 @@
 import 'package:flutter/material.dart';
+class TabTransaksiMasuk extends StatelessWidget {
+  const TabTransaksiMasuk({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+      children: [
+        Text("Semua Transaksi"),
+        InkWell(
+          child: Column(
+            children: [
+              Text('1 April 2023'),
+              Card(
+                child: Row(children: [
+                  Image(image: AssetImage('images/transkasi02.png')),
+                  Column(children: [
+                    Text('Transfer Rupian'),
+                    Text('Transfer dari BANK MANDIRI'),
+                    Text('FARHAZ NURJANANTO 09183764644')
+                  ],),
+                  Column(children: [
+                    Icon(Icons.access_time_filled_outlined),
+                    Text("+ Rp. 20.0000")
+                  ],)
+                ]),
+          ),
+            ],
+          )
+        )
+      ],
+    ));
+  }
+}
+
+
+class TabDetailPenjualan extends StatelessWidget {
+  const TabDetailPenjualan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+      children: [
+        Text("Semua Penjualan"),
+        InkWell(
+          onTap: (){
+            
+          },
+          child: Column(
+            children: [
+              Text('Juni'),
+              Card(
+                child: Row(children: [
+                  Image(image: AssetImage('images/transkasi02.png')),
+                  Column(children: [
+                    Text('Transfer Rupian'),
+                    Text('Transfer dari BANK MANDIRI'),
+                    Text('FARHAZ NURJANANTO 09183764644')
+                  ],),
+                  Column(children: [
+                    Icon(Icons.access_time_filled_outlined),
+                    Text("+ Rp. 20.0000")
+                  ],)
+                ]),
+          ),
+            ],
+          )
+        )
+      ],
+    ));
+  }
+}
+
+
+
 
 class PeternakTransaksi extends StatefulWidget {
   const PeternakTransaksi({super.key});
@@ -10,16 +86,29 @@ class PeternakTransaksi extends StatefulWidget {
 class _PeternakTransaksiState extends State<PeternakTransaksi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    
+    return DefaultTabController(length: 2, child: Scaffold(
       appBar: AppBar(
         leading: BackButton(onPressed: () {
-          
+          Navigator.pop(context);
           },),
-        title: Title(color: Colors.indigo, child: Text("Transaksi Masuk")),
+        title: Title(color: Colors.indigo, child: Text("Transaksi")),
+        bottom: TabBar(
+          tabs: [
+            Tab(child: Text("Transaksi Masuk"),),
+            Tab(child: Text("Detail Penjualan"),)
+          ]
+        ),
       ),
-      body: ListView(
-        
-      ),
-    );
+    body: TabBarView(children: [
+      TabTransaksiMasuk(),
+      TabDetailPenjualan()
+    ],)
+    ));
+    
+
   }
 }
+
+
+
